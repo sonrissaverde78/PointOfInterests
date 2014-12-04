d256069f7dbebab40e37bbfba633e285e12b38f8
sonrissaverde78 <sonrissaverde78@gmail.com>
2014-12-04 17:58:32 +0100

    Sample Working

--- src/com/pfc/pointofinterests/MainActivityPointOfInterests.java
+++ src/com/pfc/pointofinterests/MainActivityPointOfInterests.java
@@ -1,15 +1,22 @@
 package com.pfc.pointofinterests;
 
-import com.example.pointofinterests.R;
+import java.io.File;
+
+import com.pfc.pointofinterests.R;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivityPointOfInterests extends ActionBarActivity {
 
+	public static final String EXTRAS_KEY_ACTIVITY_TITLE_STRING = "activityTitle";
+	public static final String EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL = "activityArchitectWorldUrl";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +38,43 @@ public class MainActivityPointOfInterests extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+        	LaunchCamActivity();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void LaunchCamActivity ()
+    {
+		// com.pfc.pointofinterests.SampleCamHandlePoiDetailActivity
+		/* launch activity */
+		/* store className of activity to call*/
+		final String className = "com.pfc.pointofinterests.SampleCamHandlePoiDetailActivity";
+
+		try {
+
+			final Intent intent = new Intent(this, Class.forName(className));
+			intent.putExtra(EXTRAS_KEY_ACTIVITY_TITLE_STRING,
+					"POI");
+			intent.putExtra(EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL, // "samples" +
+					"5_Browsing$Pois_5_Native$Detail$Screen" //this.getArchitectWorldUrls()[position]
+					+ File.separator + "index.html");
+			/*
+			 * intent.putExtra(EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL, "samples"
+					+ File.separator + this.getArchitectWorldUrls()[position]
+					+ File.separator + "index.html");
+			 */
+
+			/* launch activity */
+			this.startActivity(intent);
+
+		} catch (Exception e) {
+			/*
+			 * may never occur, as long as all SampleActivities exist and are
+			 * listed in manifest
+			 */
+			Toast.makeText(this, className + "\nnot defined/accessible",
+					Toast.LENGTH_SHORT).show();
+		}
     }
 }
